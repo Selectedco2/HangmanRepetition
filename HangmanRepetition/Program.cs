3c78ee8dbc643512318f0492f25add2138434c0a
Andreas Thegelström <andreas.thegelstrom@elev.ga.ntig.sem>
2024-08-27 10:38:20 +0200

    Almost finished product

---
+++ HangmanRepetition/Program.cs
@@ -0,0 +1,48 @@
+﻿string word = "KokosKaka";
+
+/*
+
+Bestämma ord
+Visa hur många bokstäver med _
+Ta en ny gissning
+
+Om gissning finns i ordet:
+ Skriver på respektive plats
+
+Annars
+ Lägger till bokstaven till felaktiv gissning
+ Rita ett till steg av gubben
+
+Fortsätt tills gubben dör eller ordet gissas rätt
+
+*/
+
+List<char> wrongGuesses = [];
+List<char> hiddenWord = new() {'_', '_', '_', '_', '_', '_', '_', '_', '_'};
+
+int lives = 10;
+
+while(wrongGuesses.Count < lives)
+{
+    Console.WriteLine(string.Join(" ", hiddenWord));
+
+    // foreach (char letter in hiddenWord)
+    // {
+    //     Console.Write(letter);
+    // }
+
+    char guess = Console.ReadLine()[0];
+
+    if (word.Contains(guess) == true)
+    {
+        Console.WriteLine("Helt rätt!");
+    }
+
+    else
+    {
+        Console.WriteLine("Inte riktigt..");
+        wrongGuesses.Add(guess);
+    }
+}
+Console.WriteLine("Aj de gg");
+Console.ReadLine();
